679a38a92ed032ae9e391ef754dddd30bd59a3d3
Roger Lenke <rogerlenke@gmail.com>
2018-11-06 20:56:08 -0200

    Fixing plot style
    Co-authored-by: Durval Carvalho <durvalcsouza@outlook.com>
    Co-authored-by: Roger Lenke <rogerlenke@gmail.com>
    Co-authored-by: Igor Gabriel <igorgabriel515@gmail.com

--- UnGastos/main/graphs/detailed_nature.py
+++ UnGastos/main/graphs/detailed_nature.py
@@ -8,26 +8,42 @@ class DetailedNature():
 
     def __read_csv__(self):
         df = pd.read_csv('UnGastos/static/data/detailed_nature.csv')
-        self.df = df
+        df = df.sort_values(by=['Pago'], ascending=False)
+
+        df_greatest = df.head(10)
+
+        self.df = df_greatest
 
     def generate_graphic(self):
         self.__read_csv__()
 
         data = [
             go.Bar(
-                x=self.df['Nome Natureza Detalhada'],
-                y=self.df['Pago'],
+                x=self.df['Pago'],
+                y=self.df['Nome Natureza Detalhada'],
                 marker=dict(
                     color='rgb(158,202,225)',
                     line=dict(
                         color='rgb(8, 48 ,107)',
                         width=1.5)
-                ))
+                ),
+                orientation='h'
+            ),
         ]
+
+        layout = go.Layout(
+            title='Gastos por Natureza',
+            margin=go.layout.Margin(
+                l=325,
+            )
+        )
+
+        config = {'displayModeBar': False}
 
         plotly.offline.plot({
             "data": data,
-            "layout": go.Layout(title="Gastos por Natureza")
+            "layout": layout
             },
-            filename='UnGastos/main/templates/detailed_nature.html'
+            filename='UnGastos/main/templates/detailed_nature.html',
+            config=config
         )
