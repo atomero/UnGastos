9eb2ebd6c9c07bfbbe99fa11809f60a0efe5ddd5
Igor Gabriel <igorgabriel515@gmail.com>
2018-11-12 22:39:24 -0200

    Adding plot generation.

--- UnGastos/main/views.py
+++ UnGastos/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .graphs.detailed_nature import DetailedNature
+from .graphs.expenses_per_month import ExpensesMonth
 
 
 def home(request):
     dn = DetailedNature()
     dn.generate_graphic()
+
+    em = ExpensesMonth()
+    em.generate_graphic()
 
     return render(request, 'index.html')
