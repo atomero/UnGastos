b2d38d4fed6f7a1795c770d573f2d36b60cf614f
Roger Lenke <rogerlenke@gmail.com>
2018-11-19 10:11:26 -0200

    Adding pizza plot for resource source

--- UnGastos/main/views.py
+++ UnGastos/main/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from .graphs.detailed_nature import DetailedNature
 from .graphs.expenses_per_month import ExpensesMonth
+from .graphs.resource_source import ResourceSource
 
 
 def home(request):
     dn = DetailedNature()
     dn.generate_graphic()
 
-    # em = ExpensesMonth()
-    # em.generate_graphic()
-
     return render(request, 'index.html')
+
 
 def expanse(request):
     em = ExpensesMonth()
     em.generate_graphic()
-    
+
     return render(request, 'expanse.html')
+
+
+def resource_source(request):
+    rs = ResourceSource()
+    rs.generate_graphic()
+
+    return render(request, 'resource_source.html')
